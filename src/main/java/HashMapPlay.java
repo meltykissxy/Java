2c6828e9e79bf659017880f050f1c11072cff039
Apple <beautifully@188.com>
2021-03-02 08:12:01 +0800

    Initial commit

---
+++ src/main/java/HashMapPlay.java
@@ -0,0 +1,71 @@
+import com.apple.common.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapPlay {
+    @Test
+    public void play01() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("lss", 18);
+        map.put("ly", 16);
+        map.put("lxy", 17);
+        map.put("lxy", null);
+        System.out.println(map);//{lxy=null, lss=18, ly=16}
+    }
+    /**
+     * 不能直接修改key，只能删掉后重新增加
+     * 知识点：remove后会返回被删除的值
+     */
+    @Test
+    public void play02() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("lss", 18);
+        map.put("ly", 16);
+        map.put("lxy", 17);
+        map.put("qxy", map.remove("lxy"));
+        System.out.println(map);//{qxy=17, lss=18, ly=16}
+    }
+    /**
+     * 注意：如果把Person类的equals()和hashcode()去掉，会出现相同的key
+     */
+    @Test
+    public void play03() {
+        Map<Person, String> map = new HashMap<>();
+        map.put(new Person("ly", 16), "Apple");
+        map.put(new Person("ly", 16), "Google");
+        System.out.println(map);
+    }
+    /**
+     * 错误的用法：修改key，坚决不允许
+     * 如果你注释掉Person类的equals()和hashcode()，还能get到值。一旦加了equals()和hashcode()，就再也拿不回值了！！！
+     */
+    @Test
+    public void play04() {
+        Map<Person, String> map = new HashMap<>();
+        Person p = new Person("ly", 16);
+        map.put(p, "Apple");
+        p.setName("lxy");
+        System.out.println(map);
+        System.out.println(map.get(p));
+    }
+    @Test
+    public void play05() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("lss", 18);
+        map.put("ly", 16);
+        map.put("lxy", 17);
+        Collection<Integer> values = map.values();
+        for (Integer value : values) {
+            System.out.println(value);
+        }
+
+        Set<String> strings = map.keySet();
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " is " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return name != null ? name.equals(person.name) : person.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+}
