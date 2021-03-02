f06b2998083852f04be484dea5f05f74b01f2520
Apple <beautifully@188.com>
2021-03-02 11:30:03 +0800

    BitMap

---
+++ src/main/java/com/apple/BitMap.java
@@ -0,0 +1,28 @@
+package com.apple;
+
+/**
+ * https://time.geekbang.org/column/article/76827
+ */
+public class BitMap { // Java中char类型占16bit，也即是2个字节
+    private char[] bytes;
+    private int nbits;
+
+    public BitMap(int nbits) {
+        this.nbits = nbits;
+        this.bytes = new char[nbits/16+1];
+    }
+
+    public void set(int k) {
+        if (k > nbits) return;
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        bytes[byteIndex] |= (1 << bitIndex);
+    }
+
+    public boolean get(int k) {
+        if (k > nbits) return false;
+        int byteIndex = k / 16;
+        int bitIndex = k % 16;
+        return (bytes[byteIndex] & (1 << bitIndex)) != 0;
+    }
+}
